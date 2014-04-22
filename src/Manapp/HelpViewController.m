112ea059d61427071ba209613d7cbe793deac727
Kevil Cuong <nguyentacuong1987@gmail>
2014-04-22 16:02:53 +0700

    Change name image: Fix to slide

--- src/Manapp/HelpViewController.m
+++ src/Manapp/HelpViewController.m
@@ -40,11 +40,13 @@
     //LeftGesture
     UISwipeGestureRecognizer *leftGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(moveLeft:)];
     [leftGesture setDirection:UISwipeGestureRecognizerDirectionLeft];
+//    [self.currentImageView addGestureRecognizer:leftGesture];
     [self.view addGestureRecognizer:leftGesture];
     
     //Right Gesture
     UISwipeGestureRecognizer *rightGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(moveRight:)];
     [rightGesture setDirection:UISwipeGestureRecognizerDirectionRight];
+//    [self.currentImageView addGestureRecognizer:rightGesture];
     [self.view addGestureRecognizer:rightGesture];
     
     
@@ -113,9 +115,8 @@
         _currentPhotoIndex = kIndexHelpShowHomepage;
         image = [UIImage imageNamed:[NSString stringWithFormat:@"help_%d", _currentPhotoIndex]];
     }else{
-        _currentPhotoIndex = 1;
-        image = [UIImage imageNamed:@"help_1"];
-
+        _currentPhotoIndex = 2;
+         image = [UIImage imageNamed:[NSString stringWithFormat:@"help_%d", _currentPhotoIndex]];
     }
 
     if (IS_IPHONE_5) {
